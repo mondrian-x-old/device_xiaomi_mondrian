ae3fa4ec243fa150ffb5882903de5b42f9de36fa
Omkar Chandorkar <gotenksIN@aospa.co>
2023-06-15 14:26:05 +0900

    mondrian: overlay: Enable secure NFC functionality
    Change-Id: I4e9acc7d8ec14237512aadf100d564984aa5a896
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@ PRODUCT_PACKAGES += \
     ApertureResMondrian \
     FrameworksResMondrian \
     FrameworksResMondrianGlobal \
+    NfcResMondrian \
     SettingsProviderResMondrian \
     SettingsResMondrian \
     SystemUIResMondrian \
