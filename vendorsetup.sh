98c28db8c7f53c2c490d62701f1ba6a02b161843
NextWork123 <nextworks@protonmail.com>
2024-05-12 14:03:27 +0200

    mondrian: split files above 50m to 20m parts

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+cat vendor/xiaomi/mondrian/proprietary/vendor/lib/rfsa/adsp/libvpt_action_recognition.so.* > vendor/xiaomi/mondrian/proprietary/vendor/lib/rfsa/adsp/libvpt_action_recognition.so
+cat vendor/xiaomi/mondrian/proprietary/vendor/lib64/librelight_only.so.* > vendor/xiaomi/mondrian/proprietary/vendor/lib64/librelight_only.so
