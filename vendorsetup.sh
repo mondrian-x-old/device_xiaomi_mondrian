b95f297b86cc02964e254d6bd2106ab4bff11bbe
NextWork123 <nextworks@protonmail.com>
2024-07-15 12:08:01 +0200

    mondrian: Drop vendorsetup.sh
    - i don't need anymore that because i dropped the parts wit the last update of blobs

--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-cat vendor/xiaomi/mondrian/proprietary/vendor/lib/rfsa/adsp/libvpt_action_recognition.so.* > vendor/xiaomi/mondrian/proprietary/vendor/lib/rfsa/adsp/libvpt_action_recognition.so
-cat vendor/xiaomi/mondrian/proprietary/vendor/lib64/librelight_only.so.* > vendor/xiaomi/mondrian/proprietary/vendor/lib64/librelight_only.so
